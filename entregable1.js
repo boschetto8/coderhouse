d89a36f4681067a8121d4ec91bec893017ffcc0d
boschetto8 <83792545+boschetto8@users.noreply.github.com>
2021-05-06 22:24:25 -0300

    Delete entregable1.js

--- entregable1.js
+++
@@ -1,5 +0,0 @@
-let edad = parseInt(prompt("Ingrese cuantos años cumpliste o cumplirás este año:"));
-let anio = 2021;
-let nacimiento = (anio - edad);
-alert ("Naciste en el año"+ " "+nacimiento)
-console.log ("Naciste en el año"+ " "+nacimiento)
